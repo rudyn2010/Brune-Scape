5fff3e3d8a494b23327968eea06b1e021fb8a8e4
Rudy Nguyen <rudyn2010@gmail.com>
2023-01-30 16:15:31 -0800

    add mastery tracker to single card component

--- react-app/src/components/SingleCard/index.js
+++ react-app/src/components/SingleCard/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import DeleteCardModal from "../DeleteCardModal";
 import EditCardModal from "../EditCardModal";
+import MasteryTracker from "../MasteryTracker";
 import "./SingleCard.css"
 
 const SingleCard = ({ card }) => {
@@ -14,29 +15,32 @@ const SingleCard = ({ card }) => {
 
 
     return (
-        <div className="deck-container">
-            <div>
-                {showAnswer ? card?.answer : card?.question}
-            </div>
-            <div className="icon-bar">
-                <div
-                onClick={() => toggle()}
-                style={{cursor: "pointer"}}
-                >
-                    {
-                        showAnswer ?
-                        <i class="fa-solid fa-eye-slash"></i> :
-                        <i class="fa-solid fa-eye"></i>
-                    }
-                </div>
+        <>
+            <div className="deck-container">
                 <div>
-                    <EditCardModal card={ card }/>
+                    {showAnswer ? card?.answer : card?.question}
                 </div>
-                <div>
-                    <DeleteCardModal card={ card }/>
+                <div className="icon-bar">
+                    <div
+                    onClick={() => toggle()}
+                    style={{cursor: "pointer"}}
+                    >
+                        {
+                            showAnswer ?
+                            <i class="fa-solid fa-eye-slash"></i> :
+                            <i class="fa-solid fa-eye"></i>
+                        }
+                    </div>
+                    <div>
+                        <EditCardModal card={ card }/>
+                    </div>
+                    <div>
+                        <DeleteCardModal card={ card }/>
+                    </div>
                 </div>
             </div>
-        </div>
+            <MasteryTracker card={ card }/>
+        </>
     )
 };
 
